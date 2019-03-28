b07488a71d970a666187b0d0f0dff7784d0deb77
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-03-28 20:30:05 +0300

    Create massive.cpp

---
+++ massive.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int arr[5];
+    
+    cout << "Enter five numbers: " << endl;
+    
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    
+    cin.get();
+    return 0;
+}
