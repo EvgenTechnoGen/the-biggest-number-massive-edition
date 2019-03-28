2c5c68660219890afb23e23c420316cb51a8c3d2
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-03-28 20:38:54 +0300

    Update massive.cpp

--- massive.cpp
+++ massive.cpp
@@ -13,6 +13,16 @@ int main(int argc, const char * argv[]) {
     if (arr[0] > arr[1] && arr[0] > arr[2] && arr[0] > arr[3] && arr[0] > arr[4]) {
         cout << "The biggest number is - " << arr[0] << endl;
     }
+    
+    else if (arr[1] > arr[0] && arr[1] > arr[2] && arr[1] > arr[3] && arr[1] > arr[4]) {
+        cout << "The biggest number is - " << arr[1] << endl;
+    }
+    
+    
+    
+    
+    
+    
     cin.get();
     return 0;
 }
